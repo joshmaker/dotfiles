3040abb8774978c741582e7d19d6db7fc4d43e20
Josh West <18324+joshmaker@users.noreply.github.com>
2019-12-10 19:54:43 -0800

    Add keyboard shortcuts

---
+++ dotfiles/.zsh/07-keyboard-shortcuts.zsh
@@ -0,0 +1,10 @@
+# Used with zsh-autosuggestions
+# See also: https://coderwall.com/p/a8uxma/zsh-iterm2-osx-shortcuts
+bindkey "[D" backward-word  # ⌥ + →
+bindkey "[C" forward-word	# ⌥ + ←
+## These two requires iTerm2 keybindings
+bindkey "^[a" beginning-of-line  # ⌘ + ←
+bindkey "^[e" end-of-line        # ⌘ + →
+
+bindkey "[A" history-search-backward  # ⌥ + ↑
+bindkey "[B" history-search-forward  # ⌥ + ↓
